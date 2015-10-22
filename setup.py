07d6656fa52e27a21cae8e1d50f850b46bcab635
Amr Hassan <amr.hassan@gmail.com>
2015-10-22 13:53:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,11 +16,6 @@ setup(
         'Programming Language :: Python :: 3.5'
     ],
     install_requires=[
-        # 'six',
-        # 'strict_rfc3339',
-        # 'iso8601',
-        # 'pytz',
-        # 'timestamp',
         'prettytoml',
     ]
 )
